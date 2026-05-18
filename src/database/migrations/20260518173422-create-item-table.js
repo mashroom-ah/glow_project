571f21e5ff8da33341ec2ed9f14e344ff8916ba9
MashMash <ah.mary.nov@gmail.com>
2026-05-18 21:00:25 +0300

    Add of item table

--- src/database/migrations/20260518173422-create-item-table.js
+++ src/database/migrations/20260518173422-create-item-table.js
@@ -1,22 +1,87 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('item', {
+      item_id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+
+      user_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+
+        references: {
+          model: 'app_user',
+          key: 'user_id',
+        },
+
+        onDelete: 'CASCADE',
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      production_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+
+      shelf_life_closed: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+
+      shelf_life_open: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+
+      opened_at: {
+        type: Sequelize.DATEONLY,
+        allowNull: true,
+      },
+
+      expiration_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+
+      status: {
+        type: Sequelize.ENUM(
+          'fresh',
+          'expiring_soon',
+          'expiring',
+          'expired'
+        ),
+        allowNull: false,
+      },
+
+      is_active: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface) {
+    await queryInterface.dropTable('item');
+  },
 };
--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,6 +1,9 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const config = require('../../config/config')[process.env.NODE_ENV || 'development'];
+const config =
+  require('../../config/config')[
+    process.env.NODE_ENV || 'development'
+  ];
 
 const sequelize = new Sequelize(
   config.database,
@@ -19,8 +22,40 @@ const db = {};
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-db.AppUser = require('./appUser.model')(sequelize, DataTypes);
-db.RefreshToken = require('./refreshToken.model')(sequelize, DataTypes);
+db.AppUser = require('./appUser.model')(
+  sequelize,
+  DataTypes
+);
+
+db.RefreshToken = require('./refreshToken.model')(
+  sequelize,
+  DataTypes
+);
+
+db.Item = require('./item.model')(
+  sequelize,
+  DataTypes
+);
+
+//relstions
+
+db.AppUser.hasMany(db.RefreshToken, {
+  foreignKey: 'user_id',
+});
+
+db.RefreshToken.belongsTo(db.AppUser, {
+  foreignKey: 'user_id',
+});
+
+db.AppUser.hasMany(db.Item, {
+  foreignKey: 'user_id',
+});
+
+db.Item.belongsTo(db.AppUser, {
+  foreignKey: 'user_id',
+});
+
+// associations from models
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
